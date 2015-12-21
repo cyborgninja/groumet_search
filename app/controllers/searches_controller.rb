57352a8b809912b7b97419b28641a1a5f3c22846
masaru shinbori <masaru.shinbori@monetize.co.jp>
2015-12-22 02:34:30 +0900

    暫定の食べログクチコミチェックツール

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,10 +9,15 @@ class SearchesController < ApplicationController
     agent = Mechanize.new
     page = agent.get(params[:url])
     @url = params[:url].force_encoding("utf-8")
-    #@result = page.body
-    elements = page.search('.rvw-item__rvw-comment.js-rvw-comment')
-    innertext = elements.inner_text
-    @result = Array.new
+    @count = page.search('.rvw-item__rvw-title-target').count
+
+    @result_review_url = Array.new
+    review_url = page.search('.rvw-item__rvw-title-target')
+    review_url.each do |r|
+      @result_review_url.push r[:href]
+    end
+    
+    innertext = page.search('.rvw-item__rvw-comment.js-rvw-comment').inner_text
     @result = innertext.split("もっと見る")
   end
 
--- spec/factories/searches.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :search do
-    
-  end
-
-end
